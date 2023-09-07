6ab3c554d8aa90a501c17dfc32c6dd7edf4d83bc
albingcj <114584909+albingcj@users.noreply.github.com>
2023-09-07 17:02:03 +0530

    sql update

--- reg.sql
+++ reg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2023 at 03:41 PM
+-- Generation Time: Sep 07, 2023 at 01:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
